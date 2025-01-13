41e69a96576a9a72989aea4b94070629ba0ff2c1
ChocoFantasy <angel861107@gmail.com>
2025-01-13 09:14:31 +0800

    0113

--- myproject/React/src/App.jsx
+++ myproject/React/src/App.jsx
@@ -1,69 +1,53 @@
-import { useEffect, useForm, useState } from "react";
-function App() {
-  // unsplash
-  const api = "https://api.unsplash.com/seatch/photos";
-  const [accessKey] = "0p99NnZvv03vGtqrypmaM-LvsICLJSlhFN11Wl20y2M";
-  const [filterString, setFilterString] = useState("cafe");
-  // 建立列表用的陣列
-  const [jsonData, setJsonData] = useState([]);
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-  // 建立非同步方法，取得遠端資料
-  // 避免重新渲染，所有寫在useEffect中
+export default function App() {
+    // 驗證是否連上unsplash api
+    // https://api.unsplash.com/search/photos/?client_id=自己的accessKey
+    // https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
 
-  useEffect(() => {
-    const getPhotos = async () => {
-      console.log(`${api}?client_id=${accessKey}&query=${filterString}`);
-      //發出請求給遠端api，傳回結果
-      const result = await axios.get(
-        `${api}?client_id=${accessKey}&query=${filterString}`
-      );
+    const api = 'https://api.unsplash.com/search/photos';
+    const accessKey = '換上自己的accesskey';
+    const [filterString, setFilterString] = useState('coffee');
+    // 建立列表用的陣列
+    const [jsonData, setJsonData] = useState([]);
 
-      //全部資料
-      console.log(result);
-      //第一頁的10筆資料
-      console.log(result.data.results);
-      setJsonData(result.data.results);
+    // 建立非同步方法，取得遠端資料
+    // 避免重新渲染，所有寫在useEffect中
+    useEffect(() => {
+        const getPhotos = async () => {
+            console.log(`${api}?client_id=${accessKey}&query=${filterString}`);
+            // 發出請求給遠端api，傳回結果
+            const result = await axios.get(`${api}?client_id=${accessKey}&query=${filterString}`);
+            // 全部資料
+            // console.log(result);
+            // 顯示10筆記錄
+            console.log(result.data.results);
+            // 更新陣列列表資料
+            setJsonData(result.data.results);
+        }
+        getPhotos();
+    }, []);
 
-      //  ((preData)=>{
-      //     return [...preData,...result.data.results];
-      //   });
-      //   console.log(jsonData);
-      // } catch (error){
-      //   console.log(error);
-    };
-    getPhotos();
-  }, []);
+    // 建立顯示圖片元件
+    const ShowPhoto = () => {
+        return (
+            jsonData.map((item) => {
+                return (
+                    <div key={item.id}>
+                        <img src={item.urls.regular} alt="" width="400" height="320" style={{ objectFit: "cover" }} />                        
+                    </div>
+                )
+            })
+        )
+    }
 
-  // 建立顯示圖片元件
-  const Card = ({ item }) => {
+    // 渲染時使用JSX語法，若要使用JS語法，前後加{}
     return (
-      <img
-        src={Item.urls.regular}
-        alt=""
-        width="400"
-        height="320"
-        style={{ objectFit: "cover" }}
-      />
-    );
-  };
-
-  return (
-    <>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {jsonData.map((item) => {
-          <div key={Item.id}>
-            {/*     <img
-              src={Item.urls.regular}
-              alt=""
-              width="400"
-              height="320"
-              style={{ objectFit: "cover" }}
-            /> */}
-            <Card item={item} />
-          </div>;
-        })}
-      </div>
-    </>
-  );
+        <>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                <ShowPhoto />
+            </div>
+        </>
+    )
 }
-export default App;
